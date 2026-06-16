063cf3bc75f38d4b8903e62698d6c0fc89b3207b
Maria <mariya.sg.rp@gmail.com>
2026-06-16 15:11:26 -0500

    Feat setup supabase (#24)
    * feat: add utilities functions to work with supabase
    
    * refactor: move functions to src/database
    
    * fix: env example and script for generating types
    
    * fix: make type generation script cross-platform and update import route in proxy for updateSession func
    
    * refactor: move supabase env variables to a separate file

---
+++ src/proxy.ts
@@ -0,0 +1,10 @@
+import { type NextRequest, type NextResponse } from 'next/server';
+import { updateSession } from '@/database/proxy';
+
+export async function proxy(request: NextRequest): Promise<NextResponse> {
+  return await updateSession(request);
+}
+
+export const config = {
+  matcher: [String.raw`/((?!_next/static|_next/image|favicon.ico|.*\.(?:svg|png|jpg|jpeg|gif|webp)$).*)`],
+};
