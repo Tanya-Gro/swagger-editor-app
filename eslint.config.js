e9a19c5c1e6b2b61447dea662479b60b47423b4b
Tatiana Grosul <141400196+Tanya-Gro@users.noreply.github.com>
2026-06-16 17:07:48 +0300

    33-chore: Add Issue Templates and  ESLint Plugin (#34)
    * docs: add templates
    
    * chore: add eslint-plugin-react-hooks and eslint-plugin-react
    
    * chore: remove eslint-plugin-react

--- eslint.config.js
+++ eslint.config.js
@@ -1,6 +1,7 @@
 import js from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import nextCoreWebVitals from 'eslint-config-next/core-web-vitals';
+import reactHooks from 'eslint-plugin-react-hooks';
 import nextTypescript from 'eslint-config-next/typescript';
 import eslintPluginPrettier from 'eslint-plugin-prettier/recommended';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
@@ -33,6 +34,7 @@ export default defineConfig([
   ...typeCheckedTypescriptConfigs,
   ...nextCoreWebVitals,
   ...nextTypescript,
+  reactHooks.configs.flat.recommended,
   eslintPluginUnicorn.configs.recommended,
   eslintPluginPrettier,
 
@@ -58,7 +60,7 @@ export default defineConfig([
     settings: {
       react: { version: 'detect' },
     },
-    plugins: {},
+    plugins: { 'react-hooks': reactHooks },
     linterOptions: {
       noInlineConfig: true,
     },
