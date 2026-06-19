ea18e37aea615f3470be9f8f0256d525cb74d639
Fiodorova Maria <mariya.sg.rp@gmail.com>
2026-06-19 02:36:57 -0500

    feat: check if user is redirect authed users to homepage

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,17 @@
 import { LoginForm } from '@/components/LoginForm/LoginForm';
+import { serverClient } from '@/database/server-client';
+import { redirect } from 'next/navigation';
 
-export default function LoginPage() {
+export default async function LoginPage() {
+  const supabase = await serverClient();
+
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  if (user) {
+    redirect('/');
+  }
+
   return <LoginForm />;
 }
