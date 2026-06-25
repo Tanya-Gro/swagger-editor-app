c6d658f91109498d46513d35da667853d56f55bf
Tanya-Gro <tarpel.gta@gmail.com>
2026-06-25 23:58:24 +0300

    feat: add edditor actions component

---
+++ src/components/Editor/EditorActions/EditorActions.tsx
@@ -0,0 +1,51 @@
+'use client';
+
+import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
+import CodeOutlinedIcon from '@mui/icons-material/CodeOutlined';
+import FolderOpenOutlinedIcon from '@mui/icons-material/FolderOpenOutlined';
+import { Button, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import { type MouseEvent } from 'react';
+import type { EditorFormat } from '../Editor.types';
+
+import classNames from 'classnames/bind';
+import styles from './EditorActions.module.css';
+
+const cx = classNames.bind(styles);
+
+type EditorActionsProps = {
+  format: EditorFormat;
+  onChangeFormat: (format: EditorFormat) => void;
+  onClear: () => void;
+};
+
+export function EditorActions({ format, onChangeFormat, onClear }: EditorActionsProps) {
+  const changeFormat = (_event: MouseEvent<HTMLElement>, nextFormat: EditorFormat | null) => {
+    if (nextFormat) {
+      onChangeFormat(nextFormat);
+    }
+  };
+
+  return (
+    <header className={cx('header')}>
+      <CodeOutlinedIcon fontSize="small" />
+      <h2 className={cx('title')} id="editor-heading">
+        Swagger Editor
+      </h2>
+      <div className={cx('toolbar')}>
+        <ToggleButtonGroup aria-label="Format" exclusive onChange={changeFormat} size="small" value={format}>
+          <ToggleButton value="JSON">JSON</ToggleButton>
+          <ToggleButton value="YAML">YAML</ToggleButton>
+        </ToggleButtonGroup>
+
+        <div className={cx('action-group')}>
+          <Button size="small" startIcon={<ClearOutlinedIcon />} variant="outlined" onClick={onClear}>
+            Clear
+          </Button>
+          <Button size="small" startIcon={<FolderOpenOutlinedIcon />} variant="outlined">
+            Load
+          </Button>
+        </div>
+      </div>
+    </header>
+  );
+}
--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,10 +1,13 @@
 import { createTheme } from '@mui/material/styles';
 import { MuiButton } from './components/MuiButton';
 import { MuiIconButton } from './components/MuiIconButton';
+import { MuiToggleButton, MuiToggleButtonGroup } from './components/MuiToggleButton';
 
 export const theme = createTheme({
   components: {
     MuiButton,
     MuiIconButton,
+    MuiToggleButton,
+    MuiToggleButtonGroup,
   },
 });
