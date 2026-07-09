988088663208867297c14b773fd108c89711bd06
Fiodorova Maria <mariya.sg.rp@gmail.com>
2026-07-09 02:02:27 -0500

    feat: display validation errors in the form

--- src/components/RegistrationForm/RegistrationForm.tsx
+++ src/components/RegistrationForm/RegistrationForm.tsx
@@ -5,6 +5,8 @@ import Link from 'next/link';
 import styles from './Registration.module.css';
 import classNames from 'classnames/bind';
 import { useTranslations } from 'next-intl';
+import { registrationAction } from '@/app/registration/register-action';
+import { useActionState } from 'react';
 
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
@@ -14,17 +16,32 @@ const cx = classNames.bind(styles);
 export function RegistrationForm() {
   const t = useTranslations('REGISTRATION_PAGE');
 
+  const [state, formAction, isPending] = useActionState(registrationAction, {
+    data: null,
+    errors: null,
+    databaseError: null,
+  });
+
+  if (state.data) {
+    console.info(state.data);
+  }
+
+  if (state.databaseError) {
+    console.error(state.databaseError);
+  }
+
   return (
     <div className={cx('container')}>
       <h1 className={cx('title')}>{t('title')}</h1>
-      <form>
+      <form action={formAction}>
         <TextField
           label={t('emailLabel')}
           id="email"
           name="email"
           variant="outlined"
           fullWidth
-          helperText={t('emailHelperText')}
+          error={Boolean(state.errors?.email)}
+          helperText={state.errors?.email ?? t('emailHelperText')}
           margin="normal"
           autoComplete="email"
         />
@@ -34,16 +51,23 @@ export function RegistrationForm() {
           name="username"
           variant="outlined"
           fullWidth
-          helperText={t('usernameHelperText')}
+          error={Boolean(state.errors?.username)}
+          helperText={state.errors?.username ?? t('usernameHelperText')}
           margin="normal"
         />
-        <PasswordField label={t('passwordLabel')} name="password" helperText={t('passwordHelperText')} />
+        <PasswordField
+          label={t('passwordLabel')}
+          name="password"
+          helperText={t('passwordHelperText')}
+          error={state.errors?.password}
+        />
         <PasswordField
           label={t('repeatPasswordLabel')}
-          name="repeat-password"
+          name="repeatPassword"
           helperText={t('repeatPasswordHelperText')}
+          error={state.errors?.repeatPassword}
         />
-        <Button variant="contained" fullWidth type="submit" className={cx('button')}>
+        <Button variant="contained" fullWidth type="submit" className={cx('button')} loading={isPending}>
           {t('actionButtonText')}
         </Button>
       </form>
